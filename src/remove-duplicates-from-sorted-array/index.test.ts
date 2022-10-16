ea000828c0d2b9387df680d0e9aac3356289adcd
Pasindu P Konghawaththa <pasindudixon25@gmail.com>
2022-10-17 00:11:56 +0530

    add remove-duplicates-from-sorted-array

--- src/remove-duplicates-from-sorted-array/index.test.ts
+++ src/remove-duplicates-from-sorted-array/index.test.ts
@@ -1,9 +1,15 @@
-import {calc} from './index'
+import { removeDuplicates } from "./index";
 
-describe("test add function", () => {
-    it("should return 15 for add(10,5)", () => {
-        expect(calc(10)).toBe(20);
-    });it("should return 5 for add(2,3)", () => {
-        expect(calc(2)).toBe(4);
-    });
+describe("Remove Duplicates from Sorted Array", () => {
+  it("should return [0,1,2,3,4,5,6] as a numbers array", () => {
+    const arr = [0, 0, 0, 1, 1, 1, 2, 3, 4, 4, 5, 6, 6];
+    expect(removeDuplicates(arr)).toEqual([0, 1, 2, 3, 4, 5, 6]);
+  });
+  it("should return [0,1,2,3,4,5,6,7,8,9,10,12,20,21,22,23] as a numbers array", () => {
+    const arr = [
+      0, 0, 0, 0, 1, 2, 3, 3, 4, 5, 6, 7, 8, 8, 8, 9, 9, 9, 10, 10, 10, 12, 12,
+      20, 20, 21, 21, 21, 22, 23, 23,
+    ];
+    expect(removeDuplicates(arr)).toEqual([0,1,2,3,4,5,6,7,8,9,10,12,20,21,22,23]);
+  });
 });
--- src/remove-duplicates-from-sorted-array/index.ts
+++ src/remove-duplicates-from-sorted-array/index.ts
@@ -1,6 +1,16 @@
 
-
-export function calc(a:number){
-
-    return a+a
-}
+export function removeDuplicates(arr: number[]):number [] {
+    let pointer = 1; // use pointer to count value.
+    for (let index = 1; index < arr.length; index++) {
+      if (arr[index] != arr[index - 1]) {
+        // console.log(element + " " + arr[index - 1]);
+        arr[pointer] = arr[index]
+        // console.log(arr);
+        pointer= pointer+ 1
+      }
+    }
+    // console.log(arr.splice(pointer));
+     arr.length=pointer
+     return arr 
+  }
+  
